e74234d8fd21b41f441a2dc2fa361df17b800a31
Sandile <83593288+sandile-nkosi@users.noreply.github.com>
2023-08-02 00:25:32 +0200

    added error message for login function & saving input data feature

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,38 +1,69 @@
 const Student = require('../models/student.model');
 const authUtil = require('../util/auth');
+const sessionFlash = require('../util/session-flash');
 
 function getSignup(req, res) {
   res.render('student/auth/signup');
 }
 
-async function signup(req, res) {
+async function signup(req, res, next) {
   const student = new Student(
     req.body.email,
     req.body.password
   );
 
-  await student.signup();
+  try {
+    await student.signup();
+  } catch (error) {
+    next(error);
+    return;
+  }
+  
 
   res.redirect('/login');
 }
 
 function getLogin(req, res) {
-  res.render('student/auth/login')
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if(!sessionData) {
+    sessionData = {
+      email: '',
+      password: ''
+    }
+  }
+  res.render('student/auth/login', { inputData: sessionData });
 }
 
-async function login(req, res) {
+async function login(req, res, next) {
   const student = new Student(req.body.email, req.body.password);
-  const existingStudent = await student.getExistingStudentEmail();
+  let existingStudent;
+  try {
+    existingStudent = await student.getExistingStudentEmail(); 
+  } catch (error) {
+    next(error);
+    return;
+  }
+  
+  const sessionErrorData = {
+    errorMessage: 'Invalid credentials - please double check your email and password!', 
+    email: student.email,
+    password: student.password
+  }
 
   if (!existingStudent) {
-    res.redirect('/login')
+    sessionFlash.flashDataToSession(req, sessionErrorData, ()=> {
+      res.redirect('/login');
+    })
     return;
   }
 
   const correctPassword = await student.hasMatchingPassword(existingStudent.password);
 
   if (!correctPassword) {
-    res.redirect('/login')
+    sessionFlash.flashDataToSession(req, sessionErrorData, ()=> {
+      res.redirect('/login');
+    })
     return;
   }
 
