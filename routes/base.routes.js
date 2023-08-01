6aab8cc847404994704dba8f805a5616299d7825
Sandile <83593288+sandile-nkosi@users.noreply.github.com>
2023-08-02 01:42:42 +0200

    added a nav bar for mobile and desktop devices. Not fully functioning

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=> {
-  res.redirect('/dashboard');
+  res.redirect('/login');
 });
 
 module.exports = router;
