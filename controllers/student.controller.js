c5b4011d1fedb46cb2636a0e82371e4383a20167
Sandile <83593288+sandile-nkosi@users.noreply.github.com>
2023-08-04 01:18:44 +0200

    edited dashboard page. added a submit button

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -2,11 +2,16 @@ function getDashboard (req, res) {
   res.render('student/dashboard/student-profile');
 }
 
+function getDetails () {
+
+}
+
 function editDetails () {
 
 };
 
 module.exports = {
   getDashboard: getDashboard,
+  getDetails: getDetails,
   editDetails: editDetails
 }
--- routes/student.routes.js
+++ routes/student.routes.js
@@ -4,6 +4,7 @@ const studentController = require('../controllers/student.controller');
 const router = express.Router();
 
 router.get('/dashboard', studentController.getDashboard);
+router.get('/', studentController.getDetails);
 router.post('dashboard/edit', studentController.editDetails);
 
 module.exports = router;
