6eb49588fd1948db94731e35bbe7f1da26396a45
Sandile <83593288+sandile-nkosi@users.noreply.github.com>
2023-09-01 14:44:52 +0200

    added functionality on the registration view, to pull modules from the db

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -1,21 +1,24 @@
+const db = require('../data/database');
+
 function getDashboard (req, res) {
   res.render('student/dashboard/student-profile');
-}
+};
 
-function getRegistration (req, res) {
+async function getRegistration (req, res) {
+  const modules = await db.getDb().collection('modules').find().toArray();
   
-  res.render('student/registration/registration');
-}
+  res.render('student/registration/registration', { modules: modules });
+};
 
 function updateDashboard (req, res) {
   console.log(req.body);
   console.log(req.file);
 
   res.redirect('/dashboard');
-}
+};
 
 module.exports = {
   getDashboard: getDashboard,
   getRegistration: getRegistration,
   updateDashboard: updateDashboard
-}
+};
